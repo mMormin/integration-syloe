9eacc00e486a5d2df34c0824db2873732eacb79e
MMB-Laptop <mmboudot@gmail.com>
2023-11-18 22:04:07 +0100

    :sparkles: New functionnality : Managing active link State/style and open menu State/style

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -3,48 +3,73 @@ import {
   BsFillHeartFill,
   BsHeadphones,
   BsFillTrophyFill,
+  BsChevronDown,
 } from 'react-icons/bs';
 import './index.scss';
+import { useState } from 'react';
+
+interface NavbarLink {
+  icon: JSX.Element;
+  text: string;
+}
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isActive, setIsActive] = useState<number | null>(null);
+
+  const handleToggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const navbarLinks: NavbarLink[] = [
+    {
+      icon: <BsArrowUpRight className="navbar-link__icon" />,
+      text: 'trending',
+    },
+    {
+      icon: <BsFillHeartFill className="navbar-link__icon" />,
+      text: 'health',
+    },
+    {
+      icon: <BsHeadphones className="navbar-link__icon" />,
+      text: 'music',
+    },
+    {
+      icon: <BsFillTrophyFill className="navbar-link__icon" />,
+      text: 'reward',
+    },
+  ];
+
+  const handleClickMenu = (index: number) => {
+    setIsActive(index === isActive ? null : index);
+  };
+
   return (
-    <header className="header h-40">
-      <ul className="navbar flex flex-row justify-evenly items-center h-full px-10 bg-white">
-        <li className="navbar-link">
-          <div className="navbar-link__picture">
-            <img
-              className="messages-header__image"
-              alt="profil"
-              src="https://i.pravatar.cc/300"
-            />
-          </div>
-          <h2 className="navbar-link__text">you</h2>
-        </li>
-        <li className="navbar-link">
-          <div className="navbar-link__picture">
-            <BsArrowUpRight />
-          </div>
-          <h2 className="navbar-link__text">trending</h2>
-        </li>
-        <li className="navbar-link">
-          <div className="navbar-link__picture">
-            <BsFillHeartFill />
-          </div>
-          <h2 className="navbar-link__text">health</h2>
-        </li>
-        <li className="navbar-link">
-          <div className="navbar-link__picture">
-            <BsHeadphones />
-          </div>
-          <h2 className="navbar-link__text">music</h2>
-        </li>
-        <li className="navbar-link">
-          <div className="navbar-link__picture">
-            <BsFillTrophyFill />
-          </div>
-          <h2 className="navbar-link__text">reward</h2>
-        </li>
-      </ul>
+    <header className="header ">
+      <div className={`header-menu h-40 ${isOpen ? 'active' : ''}`}>
+        <ul className="navbar flex flex-row justify-evenly items-center h-full px-10 bg-white absolute">
+          {navbarLinks.map((link, index) => (
+            <li
+              key={link.text}
+              className={`navbar-link ${isActive === index ? 'active' : ''}`}
+            >
+              <button type="button" onClick={() => handleClickMenu(index)}>
+                <div className="navbar-link__picture">{link.icon}</div>
+                <h2 className="navbar-link__text">{link.text}</h2>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="header-button w-full z-40 relative">
+        <button
+          className="w-full flex justify-center"
+          onClick={handleToggleMenu}
+          type="button"
+        >
+          <BsChevronDown />
+        </button>
+      </div>
     </header>
   );
 }
